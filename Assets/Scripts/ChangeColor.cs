2dac985aceda4403fa2b4ad404a2d46dcd7a8271
tahubulatt <muhammadaditya.mana@gmail.com>
2024-09-24 21:26:20 +0800

    simpan file tugas AVR

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections; 
+using System.Collections.Generic; 
+using UnityEngine; 
+public class ChangeColor : MonoBehaviour
+{
+    public void Red()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
